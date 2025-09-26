a92c35c9c3380a14f87a4de536df03b893eda809
Sergey <seygorin@gmail.com>
2025-09-26 15:12:07 +0500

    fix: add env for prod

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: true,
-  geminiApiKey: 'GEMINI_API_KEY_PLACEHOLDER',
+  geminiApiKey: process.env['GEMINI_API_KEY'] || '',
   firebase: {
-    apiKey: 'FIREBASE_API_KEY_PLACEHOLDER',
-    authDomain: 'FIREBASE_AUTH_DOMAIN_PLACEHOLDER',
-    projectId: 'FIREBASE_PROJECT_ID_PLACEHOLDER',
-    storageBucket: 'FIREBASE_STORAGE_BUCKET_PLACEHOLDER',
-    messagingSenderId: 'FIREBASE_MESSAGING_SENDER_ID_PLACEHOLDER',
-    appId: 'FIREBASE_APP_ID_PLACEHOLDER',
+    apiKey: process.env['FIREBASE_API_KEY'] || '',
+    authDomain: process.env['FIREBASE_AUTH_DOMAIN'] || '',
+    projectId: process.env['FIREBASE_PROJECT_ID'] || '',
+    storageBucket: process.env['FIREBASE_STORAGE_BUCKET'] || '',
+    messagingSenderId: process.env['FIREBASE_MESSAGING_SENDER_ID'] || '',
+    appId: process.env['FIREBASE_APP_ID'] || '',
   },
 };
